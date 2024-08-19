9a762da4845f485d158abafe85904919a824fd0e
Theo Zebua <ronizebua.rz@gmail.com>
2024-08-19 19:39:15 +0700

    `rewrite:` update starter project

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-use App\Controllers\HomeController;
-use OzeFramework\Router\Route;
-
-/*
-|------------------------------------------------------------
-| Register Web Routes
-|------------------------------------------------------------
-*/
-
-$route = new Route();
-
-$route->get('/', [HomeController::class, 'index']);
-
-/*
-|------------------------------------------------------------
-| Return The Router Instance
-|------------------------------------------------------------
-*/
-
-return $route;
